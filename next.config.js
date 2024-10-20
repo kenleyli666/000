b7fce6780fd46a5bc64864f92e83e759b10e4010
kenleyli666 <107999510+kenleyli666@users.noreply.github.com>
2024-10-20 23:51:12 +0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,26 @@
+
+import withPWA from 'next-pwa';
+import runtimeCaching from 'next-pwa/cache.js';
+
+const nextConfig = withPWA({
+    pwa: {
+        dest: 'public',
+        register: true,
+        skipWaiting: true,
+        disable: process.env.NODE_ENV === 'development',
+        runtimeCaching,
+    },
+});
+
+export default {
+    ...nextConfig,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'kenleyli666.github.io',
+                pathname: '/booksApi/**',
+            },
+        ],
+    },
+};
