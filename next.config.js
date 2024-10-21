1b7c864a690c278f4993799486a9e038baa21248
kenleyli666 <107999510+kenleyli666@users.noreply.github.com>
2024-10-21 14:54:33 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,37 @@
+// import withPWA from 'next-pwa';
+// import runtimeCaching from 'next-pwa/cache.js';
 
-import withPWA from 'next-pwa';
-import runtimeCaching from 'next-pwa/cache.js';
+// const nextConfig = withPWA({
+//     pwa: {
+//         dest: 'public',
+//         register: true,
+//         skipWaiting: true,
+//         disable: process.env.NODE_ENV === 'development',
+//         runtimeCaching,
+//     },
+// });
 
-const nextConfig = withPWA({
-    pwa: {
-        dest: 'public',
-        register: true,
-        skipWaiting: true,
-        disable: process.env.NODE_ENV === 'development',
-        runtimeCaching,
-    },
-});
+// export default {
+//     ...nextConfig,
+//     images: {
+//         remotePatterns: [
+//             {
+//                 protocol: 'https',
+//                 hostname: 'kenleyli666.github.io',
+//                 pathname: '/booksApi/**',
+//             },
+//         ],
+//     },
+// };
 
-export default {
-    ...nextConfig,
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'kenleyli666.github.io',
-                pathname: '/booksApi/**',
-            },
-        ],
-    },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'kenleyli666.github.io' },
+    ],
+  },
 };
+
+export default nextConfig;
+
